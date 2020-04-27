59e8e7a8ce900ed4b0a5ba5017ac21ce4938cb58
gaurangsabane <64184425+gaurangsabane@users.noreply.github.com>
2020-04-27 10:35:18 +0530

    new vb file create

---
+++ sample.vb
@@ -0,0 +1,16 @@
+Module Module1
+    Sub Main()
+        Dim a,b,r As Interger
+        System.Console.WriteLine("Enter A: ")
+        a=System.Console.ReadLine()
+        System.Console.WriteLine("Enter B: ")
+        b=System.Console.ReadLine()
+        r=a+b
+        System.Console.WriteLine("Result: "&r)
+        System.Console.ReadKey()
+    End Sub
+End Module
+
+  
+        
+
